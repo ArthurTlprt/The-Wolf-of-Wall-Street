60e9390397a8c38b2b1e44c6228b78c23222e9cb
ArthurTlprt <arthur.talpaert@isen-lille.fr>
2016-11-24 11:31:01 +0100

    gribouillage de maternelle

--- pattern_recognition.py
+++ pattern_recognition.py
@@ -23,33 +23,36 @@ date_converter = bytedate2num("%Y%m%d%H%M%S")
 
 date,bid,ask = np.loadtxt('GBPUSD1d.txt', unpack=True,delimiter=',',converters={0: date_converter})
 
-patternAr = []
-performanceAr = []
-avgLine = ((bid+ask)/2)
-patForRec = []
-patternSize = 30
+# def graphRawFX():
+#
+#     fig=plt.figure(figsize=(10,7))
+#
+#     ax1 = plt.subplot2grid((40,40), (0,0), rowspan=40, colspan=40)
+#     ax1.plot(date,bid)
+#     ax1.plot(date,ask)
+#     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
+#     #####
+#     plt.grid(True)
+#     for label in ax1.xaxis.get_ticklabels():
+#             label.set_rotation(45)
+#     plt.gca().get_yaxis().get_major_formatter().set_useOffset(False)
+#
+#     #######
+#     ax1_2 = ax1.twinx()
+#
+#     ax1_2.plot(date, (ask-bid))
+#
+#     ax1_2.fill_between(date, 0, (ask-bid), facecolor='g',alpha=.3)
+#
+#     ax1_2.set_ylim(0, 3*ask.max())
+#     #######
+#
+#     plt.subplots_adjust(bottom=.23)
+#     #plt.grid(True)
+#
+#     plt.show()
 
 
-def patternRecognition():
-    for eachPattern in patternAr:
-        sim = []
-        for i in range(patternSize):
-            sim.append(100.00 - abs(percentChange(eachPattern[i], patForRec[i])))
-        howSim = sum(sim)/10.00
-        if howSim > 90:
-            patdex = patternAr.index(eachPattern)
-            print("#######################################")
-            print(patForRec)
-            print("=======================================")
-            print(eachPattern )
-            print("=======================================")
-            print("predicted outcome", performanceAr[patdex])
-            xp = range(1, patternSize+1)
-            fig = plt.figure()
-            plt.plot(xp, patForRec)
-            plt.plot(xp, eachPattern)
-            plt.show()
-            print("#######################################")
 
 
 def currentPattern():
@@ -90,40 +93,46 @@ def patternStorage():
     print( len(performanceAr) )
     print('pattern finder took :', patEndTime - patStartTime, ' seconds')
 
-def graphRawFX():
 
-    fig=plt.figure(figsize=(10,7))
+def patternRecognition():
+    patFound = 0
+    plotPatAr = []
+    for eachPattern in patternAr:
+        sim = []
+        for i in range(patternSize):
+            sim.append(100.00 - abs(percentChange(eachPattern[i], patForRec[i])))
+            howSim = sum(sim)/patternSize
+        if howSim > 70:
+            patFound = 1
+            patdex = patternAr.index(eachPattern)
+            plotPatAr.append(eachPattern)
+            xp = range(1, patternSize+1)
 
-    ax1 = plt.subplot2grid((40,40), (0,0), rowspan=40, colspan=40)
-    ax1.plot(date,bid)
-    ax1.plot(date,ask)
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M:%S'))
-    #####
-    plt.grid(True)
-    for label in ax1.xaxis.get_ticklabels():
-            label.set_rotation(45)
-    plt.gca().get_yaxis().get_major_formatter().set_useOffset(False)
+    if patFound == 1:
+        fig = plt.figure()
+        for eachPat in plotPatAr:
+            plt.plot(xp, eachPat)
+        plt.plot(xp, patForRec, '#92252c', linewidth = 3)
+        plt.grid(True)
+        plt.title("Pattern recognition")
+        plt.show()
 
-    #######
-    ax1_2 = ax1.twinx()
+dataLength = int(bid.shape[0])
+patternSize = 30
+toWhat = 37000
 
-    ax1_2.plot(date, (ask-bid))
-
-    ax1_2.fill_between(date, 0, (ask-bid), facecolor='g',alpha=.3)
-
-    ax1_2.set_ylim(0, 3*ask.max())
-    #######
-
-    plt.subplots_adjust(bottom=.23)
-    #plt.grid(True)
-
-    plt.show()
-
-
-
-patternStorage()
-currentPattern()
-patternRecognition()
-endTotaltime = time.time()
-print(endTotaltime-startTime)
+print("dataLength: ", dataLength)
+while toWhat < dataLength:
+    avgLine = ((bid+ask)/2)
+    avgLine = avgLine[:toWhat]
+    patternAr = []
+    performanceAr = []
+    patForRec = []
+    toWhat +=1
+    patternStorage()
+    currentPattern()
+    patternRecognition()
+    endTotaltime = time.time()
+    print(endTotaltime-startTime)
+    moveOn = input("press enter")
 # video 9...
